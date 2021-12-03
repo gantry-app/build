49d7137ee056f8acf2f003d89fcb709c89b3b8ba
Julian Meyer <julianmeyer2000@gmail.com>
2021-12-03 09:01:03 -0800

    Send request to backend on build

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
+import axios from "axios";
+
+const BACKEND_API_URL = "https://dev.gantry.app/api";
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const authToken = core.getInput('authToken');
-    core.info(`Got auth token with length ${authToken.length}`);
+    const authToken = core.getInput("authToken");
+
+    const res = await axios.post(
+      "/builds",
+      {},
+      {
+        baseURL: BACKEND_API_URL,
+        headers: { Authorization: `Bearer ${authToken}` },
+      }
+    );
+
+    core.info(`Created build ${res.data.id}`);
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
     }
-    core.setFailed('Unknown failure')
+    core.setFailed("Unknown failure");
   }
 }
 
